750e0cbaf384b587cf814a47f9af9a36ccb25222
pingzing <mcalistern@gmail.com>
2015-09-04 00:59:03 +0300

    Add file validation

--- CodeStore8UI/CodeStore8UI.Windows/MainPage.xaml.cs
+++ CodeStore8UI/CodeStore8UI.Windows/MainPage.xaml.cs
@@ -1,26 +1,17 @@
 ﻿using CodeStore8UI.Common;
 using CodeStore8UI.Model;
 using CodeStore8UI.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CodeStore8UI
-{    
+{
     public sealed partial class MainPage : BindablePage
     {
         public MainPage()
