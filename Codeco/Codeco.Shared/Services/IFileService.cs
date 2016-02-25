b788523e719aa9d37af5957a5f844b1e6a8ff832
pingzing <neil.mcalister@futurice.com>
2016-02-25 13:18:25 +0100

    see prevoius

---
+++ Codeco/Codeco.Shared/Services/IFileService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Codeco.Model;
+
+namespace Codeco.Services
+{
+    public interface IFileService : IService
+    {
+        Task StopRoamingFile(IBindableStorageFile file);
+        Task RoamFile(IBindableStorageFile file);
+        IReadOnlyList<IBindableStorageFile> GetLocalFiles();
+        IReadOnlyList<IBindableStorageFile> GetRoamedFiles();
+
+        /// <summary>
+        /// Saves and encrypts the contents into a StorageFile with the given name, and the given password.
+        /// </summary>
+        /// <param name="contents">The contents to encrypt.</param>
+        /// <param name="fileName">The filename with which to save the file.</param>
+        /// <param name="password">The password that will be used to generate the encryption key.</param>
+        /// <returns>If successful, returns the created StorageFile.</returns>
+        Task<BindableStorageFile> SaveAndEncryptFileAsync(string contents, string fileName, string password);
+        Task<string> RetrieveFileContentsAsync(string fileName, string password, FileService.FileLocation location);
+        Task ClearFileAsync(string name, FileService.FileLocation location);
+        Task DeleteFileAsync(StorageFile backingFile, FileService.FileLocation location);
+        Task RenameFileAsync(IBindableStorageFile file, string newName);
+        Task NukeFiles();
+        FileService.FileLocation GetFileLocation(BindableStorageFile file);
+    }
+}
---
+++ Codeco/Codeco.Windows10/SettingsPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Codeco.Common;
+using Codeco.Model;
+using Codeco.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Codeco
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SettingsPage : BindablePage
+    {
+        public SettingsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {            
+            var tappedItem = (sender as ToggleSwitch)?.Tag as BindableStorageFile;            
+            if (tappedItem == null)
+            {
+                return;
+            }
+
+            var context = (DataContext as SettingsViewModel);
+            if (!tappedItem.IsRoamed && !context.FileGroups.First(x => x.Location == FileService.FileLocation.Local).Files.Contains(tappedItem))
+            {
+                context?.RemoveFileFromSyncCommand.Execute(tappedItem);
+            }
+
+            if (tappedItem.IsRoamed && !context.FileGroups.First(x => x.Location == FileService.FileLocation.Roamed).Files.Contains(tappedItem))
+            {
+                context?.SyncFileCommand.Execute(tappedItem);
+            }            
+        }        
+    }
+}
