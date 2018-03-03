b844efaa83d189a4f7e7328707ccf67858e2e734
pingzing <mcalistern@gmail.com>
2018-03-03 15:42:49 +0200

    Fix android build script some more

--- Codeco.CrossPlatform.Android/appcenter-post-clone.sh
+++ Codeco.CrossPlatform.Android/appcenter-post-clone.sh
@@ -19,5 +19,5 @@ fi
 
 echo "Replacing API keys in App.xaml.cs."
 
-sed --in-place .bak --expression="s/<UwpReplaceMe>/$uwp_api_key" ../Codeco.CrossPlatform/App.xaml.cs \
-| sed --in-place .bak --expression="s/<AndroidReplaceMe>/$android_api_key" ../Codeco.CrossPlatform/App.xaml.cs
+sed -i .bak -e "s/<UwpReplaceMe>/$uwp_api_key" ../Codeco.CrossPlatform/App.xaml.cs \
+| sed -i .bak -e "s/<AndroidReplaceMe>/$android_api_key" ../Codeco.CrossPlatform/App.xaml.cs
