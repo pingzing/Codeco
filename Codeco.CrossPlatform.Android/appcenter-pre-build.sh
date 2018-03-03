d847fedb26732698f8358dbf040732b968d9d445
pingzing <mcalistern@gmail.com>
2018-03-03 18:18:31 +0200

    Change android post-clone to pre-build to fix wacky missing file issue

---
+++ Codeco.CrossPlatform.Android/appcenter-pre-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+uwp_api_key=$UWP_API_KEY
+android_api_key=$ANDROID_API_KEY
+
+echo "Checking UWP API key."
+
+if [ -z "$uwp_api_key" ] ; then
+    echo "No UWP API key found under UWP_API_KEY environment variable." 1>&2    
+    exit 1
+fi
+
+echo "Checking Android API key."
+
+if [ -z $android_api_key ] ; then
+    echo "No Android API key found under ANDROID_API_KEY environment variable." 1>&2
+    exit 1
+fi
+
+echo "Replacing API keys in App.xaml.cs."
+
+sed -i .bak -e "s/<UwpReplaceMe>/$uwp_api_key/" ../Codeco.CrossPlatform/App.xaml.cs | sed -i .bak -e "s/<AndroidReplaceMe>/$android_api_key/" ../Codeco.CrossPlatform/App.xaml.cs
