dd8e8f6608cd067ec7aeae01476e015409345f16
pingzing <mcalistern@gmail.com>
2018-03-03 15:35:38 +0200

    Fix android build script some more

--- Codeco.CrossPlatform.Android/appcenter-post-clone.sh
+++ Codeco.CrossPlatform.Android/appcenter-post-clone.sh
@@ -3,20 +3,21 @@
 uwp_api_key=$UWP_API_KEY
 android_api_key=$ANDROID_API_KEY
 
+echo "Checking UWP API key."
+
 if [ -z "$uwp_api_key" ] ; then
-    echo "UWP API key is null." 1>&2    
+    echo "No UWP API key found under UWP_API_KEY environment variable." 1>&2    
     exit 1
 fi
 
-if [ -n $uwp_api_key ] ; then
-    echo "UWP API key is empty." 1&>2
-    exit 1
-fi
+echo "Checking Android API key."
 
-if [ -z $android_api_key ] || [ -n $android_api_key ] ; then
+if [ -z $android_api_key ] ; then
     echo "No Android API key found under ANDROID_API_KEY environment variable." 1>&2
     exit 1
 fi
+
+echo "Replacing API keys in App.xaml.cs."
 
 # The two single quotes are there to deal with a quirk of OSX's version of sed: https://stackoverflow.com/questions/4247068/sed-command-with-i-option-failing-on-mac-but-works-on-linux
 sed --in-place '' --expression="s/<UwpReplaceMe>/$uwp_api_key" ../Codeco.CrossPlatform/App.xaml.cs \
